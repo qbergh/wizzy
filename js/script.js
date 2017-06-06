816b872e0efae2f7309e174c7089ca2bc56d9607
xPillow <Quinten@MacBook-Pro-van-Quinten-2.local>
2017-06-06 16:30:58 +0200

    loading v 1
    v1

---
+++ js/script.js
@@ -0,0 +1,196 @@
+/*function homeHandler(){
+    $('.gallery').removeClass('2_grid');
+    $('.gallery').removeClass('3_grid');
+    $('.gallery').removeClass('4_grid');
+    
+    if($(window).width() > 1400) {
+            $('.gallery').addClass('4_grid');
+        } else if(($(window).width() < 1400) && ($(window).width() > 760)) {
+            $('.gallery').addClass('3_grid');
+        } else if(($(window).width() < 760)){
+            $('.gallery').addClass('2_grid');
+        } else{
+            $('.gallery').addClass('3_grid'); 
+        }
+    
+    var galClass = $('.gallery').attr('class');
+        
+    var itemCount;
+    var intPlus;
+    
+    switch(galClass){
+        case 'gallery 2_grid':      intPlus = 2;
+                                    $('.gallery_item').css('width','45%');
+                                    $('.gallery_item').css('margin-width','5%');
+                                    break;
+            
+        case 'gallery 3_grid':      intPlus = 3;
+                                    $('.gallery_item').css('width','30.333333%');
+                                    $('.gallery_item').css('margin-width','3%');
+                                    break;
+            
+        case 'gallery 4_grid':      intPlus = 4;
+                                    $('.gallery_item').css('width','23%');
+                                    $('.gallery_item').css('margin-width','2%');
+                                    break;
+                   };
+    
+    // remove margin-right on 2nd / 3rd / 4th item
+    var checkTypeFilter = $('.homeNormalBar .selected').attr('data-filter');
+    
+    switch(checkTypeFilter){
+        case '.series':     
+                                    itemCount = $('.gallery_item.series').length;
+                                    break;
+        case '.films':              
+                                    itemCount = $('.gallery_item.films').length;
+                                    break;
+                          }
+    
+    for(var i = intPlus; i <= itemCount; i = i + intPlus ){
+        $('.gallery_item:nth-of-type('+i+')').css('margin-right','0px');
+        $('.gallery_item:nth-of-type('+i+')').css('background-color','pink');
+    }
+}*/
+
+
+function resizeSeries(){
+    /* Resizing episodes div */
+    
+    if($(window).width() < 911){
+        $('.serieInfo').addClass('hidden');
+    } else if($(window).width() > 910){
+        $('.serieInfo').removeClass('hidden');
+    }
+    
+    $('.afleveringen').removeClass('2_grid');
+    $('.afleveringen').removeClass('3_grid');
+    $('.afleveringen').removeClass('4_grid');
+
+    
+    if($(window).width() > 1400) {
+            $('.afleveringen').addClass('4_grid'); 
+        } else if(($(window).width() < 1400) && ($(window).width() > 760)) {
+            $('.afleveringen').addClass('3_grid');
+        } else if(($(window).width() < 760)){
+            $('.afleveringen').addClass('2_grid');
+        } else{
+            $('.afleveringen').addClass('3_grid');
+        }
+    
+    var aflClass = $('.afleveringen').attr('class');
+    
+    switch(aflClass){
+        case 'afleveringen 2_grid': 
+                                    var widthDiv = $('.aflList').width();
+                                    var widthDiv = widthDiv - (40 * 1);
+                                    var newDivWidth = widthDiv / 2;
+                                    $('.aflList div').each(function(){
+                                        $(this).css('width',newDivWidth);
+                                        $(this).css('height',newDivWidth);
+                                    });
+                                    $('.aflList').css('grid-template-columns','repeat(2,1fr)');
+                                    break;
+            
+        case 'afleveringen 3_grid': 
+                                    var widthDiv = $('.aflList').width();
+                                    var widthDiv = widthDiv - (40 * 2);
+                                    var newDivWidth = widthDiv / 3;
+                                    $('.aflList div').each(function(){
+                                        $(this).css('width',newDivWidth);
+                                        $(this).css('height',newDivWidth);
+                                    });
+                                    $('.aflList').css('grid-template-columns','repeat(3,1fr)');
+                                    break;
+            
+        case 'afleveringen 4_grid':  
+                                    var widthDiv = $('.aflList').width();
+                                    var widthDiv = widthDiv - (40 * 3);
+                                    var newDivWidth = widthDiv / 4;
+                                    $('.aflList div').each(function(){
+                                        $(this).css('width',newDivWidth);
+                                        $(this).css('height',newDivWidth);
+                                    });
+                                    $('.aflList').css('grid-template-columns','repeat(4,1fr)');
+                                    break;
+                   };
+    }
+
+function popUp(){
+    
+}
+
+function main() {
+
+    $('.normalBar .fa.fa-star-o').on('click',function(){
+        $(this).toggleClass('filledStar');
+     });
+    
+    /* Resizing fixed header - search bar */
+    var headerWidth = $("main").width();
+    $("header").width(headerWidth);
+    
+    
+    /* pas grootte aan op serie pagina */
+    resizeSeries();
+    
+    
+    /* initialize focus on searchBar - FAQ 
+    $(".searchBar").on('click',function(){
+       $("#searchFAQ").focus(); 
+    });*/
+    
+    /* Change line-height automatically */
+    
+    var searchingP = document.getElementsByTagName('p');
+    var numberPs = searchingP.length;
+    for(var i = 0; i < numberPs; i++){
+       var style = window.getComputedStyle(searchingP[i]),
+           currentLH = style.getPropertyValue('line-height'),
+           currentLH = parseInt(currentLH.slice(0,-2)),
+           newLH = currentLH + (currentLH /2); 
+        searchingP[i].style.lineHeight = newLH+"px";
+
+    };
+    
+    /* change profile */
+    localStorage.setItem('currentProfile','url(../img/profielfoto.jpg)');
+    var currentProfile = localStorage.getItem('currentProfile');
+    
+    $('.secondProfile').on('click',changeProfile);
+    
+    $('.thirdProfile').on('click',changeProfile);
+    
+    function changeProfile(){
+        var newProfile = $(this).css('background-image');
+        $(this).css('background-image',currentProfile);
+        $('.currentProfile').css('background-image',newProfile);
+        $('.profielfoto').css('background-image',newProfile);
+        
+    }
+    
+    /* tussentijdse oplossing */
+    
+    $('.suggesties .navUL li:nth-of-type(2)').on('click',function(){
+        $('.suggesties').toggleClass('hidden');
+        $('.aanpassenSuggesties').toggleClass('hidden');
+    })
+    
+    $('.aanpassenSuggesties .navUL li:nth-of-type(1)').on('click',function(){
+        $('.suggesties').toggleClass('hidden');
+        $('.aanpassenSuggesties').toggleClass('hidden');
+    })
+    
+    
+}
+window.onload = function() {
+    main();
+    resizeSeries();
+};
+
+window.onresize = function(){
+    var headerWidth = $("main").width();
+    $("header").width(headerWidth);
+    
+    resizeSeries();
+};
